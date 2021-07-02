72f49568f27eb4547fd7146a8b97ed26d9a1f3cf
Kuldeep Saxena <kuldepsaxena155@gmail.com>
2021-07-02 23:21:36 +0530

    Update flutter_searchbox/lib/flutter_searchbox.dart

--- flutter_searchbox/lib/flutter_searchbox.dart
+++ flutter_searchbox/lib/flutter_searchbox.dart
@@ -1777,7 +1777,7 @@ class SearchBox<S, ViewModel> extends SearchDelegate<String?> {
   /// Data must be in form of Elasticsearch response.
   final List<Map>? results;
 
-  /// String [optional] This prop returns only the distinct value documents for the specified field.
+  /// This prop returns only the distinct value documents for the specified field.
   /// It is equivalent to the DISTINCT clause in SQL. It internally uses the collapse feature of Elasticsearch.
   /// You can read more about it over here - https://www.elastic.co/guide/en/elasticsearch/reference/current/collapse-search-results.html
   final String? distinctField;
