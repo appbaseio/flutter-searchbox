23b4975f7e9c6b1c5f73e39876d9dfc813b3cd15
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-01-15 15:23:55 +0530

    final commit

--- flutter_searchbox_ui/lib/src/components/selected_filters.dart
+++ flutter_searchbox_ui/lib/src/components/selected_filters.dart
@@ -243,6 +243,7 @@ class _SelectedFiltersState extends State<SelectedFilters> {
   void setSelectedFilters() {
     try {
       final activeWidgets = this.activeWidgets;
+
       for (var id in activeWidgets.keys) {
         if (widget.subscribeTo != null && widget.subscribeTo!.isNotEmpty) {
           if (!widget.subscribeTo!.contains(id)) {
