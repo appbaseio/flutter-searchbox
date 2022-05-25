e43591b8d06971646f10850237673cc68e5b4e9c
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-05-25 20:03:00 +0530

    fix(searchbase): add missing doc ref for properties in searchStateContoller.dart

--- searchbase/lib/src/searchStateController.dart
+++ searchbase/lib/src/searchStateController.dart
@@ -5,21 +5,133 @@ import 'constants.dart';
 
 /// It stores the state of the SearchController at any time.
 class SearchControllerState {
+  /// An object that contains the aggregations data for [QueryType.term] queries.
   final Aggregations? aggregationData;
+
+  /// It is an object that represents the elasticsearch query response.
   final Results? results;
+
+  /// It represents the current status of the request.
   final RequestStatus? requestStatus;
+
+  /// Represents the error response returned by elasticsearch.
+
   final dynamic error;
+
+  /// Represents the value for a particular [QueryType].
+  ///
+  /// Depending on the query type, the value format would differ.
+  /// You can refer to the different value formats over [here](https://docs.appbase.io/docs/search/reactivesearch-api/reference#value).
   final dynamic value;
+
+  /// Represents a map of reactivesearch queries associated to the widget.
   final List<Map<dynamic, dynamic>>? query;
+
+  /// The index field(s) to be connected to the component’s UI view.
+  ///
+  /// It accepts an `List<String>` in addition to `<String>`, which is useful for searching across multiple fields with or without field weights.
+  ///
+  /// Field weights allow weighted search for the index fields. A higher number implies a higher relevance weight for the corresponding field in the search results.
+  /// You can define the `dataField` property as a `List<Map>` of to set the field weights. The object must have the `field` and `weight` keys.
+  /// For example,
+  /// ```dart
+  /// [
+  ///   {
+  ///     'field': 'original_title',
+  ///     'weight': 1
+  ///   },
+  ///   {
+  ///     'field': 'original_title.search',
+  ///     'weight': 3
+  ///   },
+  /// ]
+  /// ```
   final dynamic dataField;
+
+  /// Number of suggestions and results to fetch per request.
   final int? size;
+
+  /// To define from which page to start the results, it is important to implement pagination.
+
   final int? from;
+
+  /// Useful for showing the correct results for an incorrect search parameter by taking the fuzziness into account.
+  ///
+  /// For example, with a substitution of one character, `fox` can become `box`.
+  /// Read more about it in the elastic search https://www.elastic.co/guide/en/elasticsearch/guide/current/fuzziness.html.
   final dynamic fuzziness;
+
+  /// It allows to define fields to be included in search results.
+
   final List<String>? includeFields;
+
+  /// It allows to define fields to be excluded in search results.
   final List<String>? excludeFields;
+
+  /// Sorts the results by either [SortType.asc], [SortType.desc] or [SortType.count] order.
+  ///
+  /// Please note that the [SortType.count] is only applicable for [QueryType.term] type of search widgets.
   final SortType? sortBy;
+
+  /// Here, we are specifying that the results should update whenever one of the blacklist items is not present and simultaneously any one of the city or topics matches.
   final Map<String, dynamic>? react;
+
+  /// It is a callback function that takes the [SearchController] instance as parameter and **returns** the data query to be applied to the source component, as defined in Elasticsearch Query DSL, which doesn't get leaked to other components.
+  ///
+  /// In simple words, `defaultQuery` is used with data-driven components to impact their own data.
+  /// It is meant to modify the default query which is used by a component to render the UI.
+  ///
+  ///  Some of the valid use-cases are:
+  ///
+  ///  -   To modify the query to render the `suggestions` or `results` in [QueryType.search] type of components.
+  ///  -   To modify the `aggregations` in [QueryType.term] type of components.
+  ///
+  ///  For example, in a [QueryType.term] type of component showing a list of cities, you may only want to render cities belonging to `India`.
+  ///
+  ///```dart
+  /// Map (SearchController searchController) => ({
+  ///   		'query': {
+  ///   			'terms': {
+  ///   				'country': ['India'],
+  ///   			},
+  ///   		},
+  ///   	}
+  ///   )
+  ///```
   final Map Function(SearchController searchController)? defaultQuery;
+
+  /// It takes [SearchController] instance as parameter and **returns** the query to be applied to the dependent widgets by `react` prop, as defined in Elasticsearch Query DSL.
+  ///
+  /// For example, the following example has two components **search-widget**(to render the suggestions) and **result-widget**(to render the results).
+  /// The **result-widget** depends on the **search-widget** to update the results based on the selected suggestion.
+  /// The **search-widget** has the `customQuery` prop defined that will not affect the query for suggestions(that is how `customQuery` is different from `defaultQuery`)
+  /// but it'll affect the query for **result-widget** because of the `react` dependency on **search-widget**.
+  ///
+  /// ```dart
+  /// SearchWidgetConnector(
+  ///   id: "search-widget",
+  ///   dataField: ["original_title", "original_title.search"],
+  ///   customQuery: (SearchController searchController) => ({
+  ///     'timeout': '1s',
+  ///      'query': {
+  ///       'match_phrase_prefix': {
+  ///         'fieldName': {
+  ///           'query': 'hello world',
+  ///           'max_expansions': 10,
+  ///         },
+  ///       },
+  ///     },
+  ///   })
+  /// )
+  ///
+  /// SearchWidgetConnector(
+  ///   id: "result-widget",
+  ///   dataField: "original_title",
+  ///   react: {
+  ///    'and': ['search-component']
+  ///   }
+  /// )
+  /// ```
   final Map Function(SearchController searchController)? customQuery;
 
   SearchControllerState({
