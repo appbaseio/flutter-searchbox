731b3ad265aeb406afc2ec05493d846223436916
Siddharth Kothari <sids.aquarius@gmail.com>
2025-03-01 11:29:50 +0530

    chore (example): removes API key

--- flutter_searchbox_ui/example/ios/Runner/AppDelegate.swift
+++ flutter_searchbox_ui/example/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyA9JzjtHeXg_C_hh_GdTBdLxREWdj3nsOU")
+    GMSServices.provideAPIKey("API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
